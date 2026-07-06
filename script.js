3bf795a874c34ede0c1ebefd3fee6d5eca9bca9c
shivendragupta1 <shivendragupta996@gmail.com>
2026-07-06 07:38:06 +0530

    Add button click event to show alert message

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// 1. कंप्यूटर को बताओ कि हमारा बटन और कार्ड कहाँ है
+const meraButton = document.getElementById('html-css-btn');
+const meraCard = document.getElementById('web-project');
 
+// 2. जब कोई इस बटन पर उंगली से टच (Click) करे
+meraButton.addEventListener('click', function() {
+    
+    // 3. जादू! स्क्रीन पर एक मैसेज दिखाओ
+    alert("बटन ने काम कर दिया शिवेंद्र भाई!");
+    
+});
